fb2f05fcd6f0bc3c868bf5f97347dd0aa2273f89
Mohit Kaushik <mohitd.24.beai@acharya.ac.in>
2025-05-26 12:32:14 +0530

    Create #charactergrid.py

---
+++ #charactergrid.py
@@ -0,0 +1,16 @@
+#charactergrid
+print("Mohit,USN:1AY24AI071,SEC:O")
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+for y in range(len(grid[0])):
+    for x in range(len(grid)):
+        print(grid[x][y], end='')
+    print()
